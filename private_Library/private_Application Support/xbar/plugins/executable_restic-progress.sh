87c84587bb12003b30049d0e0c68e70fcc6fe42f
Sylvia van Os <sylvia@hackerchick.me>
2021-11-23 15:56:13 +0100

    Backup fixes

--- private_Library/private_Application Support/xbar/plugins/executable_restic-progress.sh
+++ private_Library/private_Application Support/xbar/plugins/executable_restic-progress.sh
@@ -1,2 +1,7 @@
-#!/bin/sh
-echo "Backup: $(grep -o "\d.\d*%" /tmp/restic_progress | tail -1)"
+#!/bin/bash
+if pgrep restic >/dev/null; then
+  progress="$(grep -o "\d*.\d*%" /tmp/restic_progress | tail -1)"
+  echo "Backup: ${progress:-"..."}"
+else
+  echo
+fi
