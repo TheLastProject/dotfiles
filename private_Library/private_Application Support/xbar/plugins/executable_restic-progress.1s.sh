f721921aa7210c60bc6d090c4bd45c22d95f8f02
Sylvia van Os <sylvia@hackerchick.me>
2022-03-17 09:34:14 +0100

    Tab completion fixes

--- private_Library/private_Application Support/xbar/plugins/executable_restic-progress.1s.sh
+++ private_Library/private_Application Support/xbar/plugins/executable_restic-progress.1s.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if pgrep restic >/dev/null; then
+if lsof /tmp/restic-$UID.lock >/dev/null; then
   progress="$(grep -ao "[0-9]*.[0-9]*%" /tmp/restic_progress | tail -1)"
   echo "Backup: ${progress:-"..."}"
 else
