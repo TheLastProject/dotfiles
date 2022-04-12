02523558d78bccf123d8d79dfb815661622b01c1
Sylvia van Os <sylvia@hackerchick.me>
2022-04-12 14:09:55 +0200

    Fix false positive matches

--- private_Library/private_Application Support/xbar/plugins/executable_restic-progress.1s.sh
+++ private_Library/private_Application Support/xbar/plugins/executable_restic-progress.1s.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if lsof /tmp/restic-$UID.lock >/dev/null; then
-  progress="$(grep -ao "[0-9]*.[0-9]*%" /tmp/restic_progress | tail -1)"
+  progress="$(grep -ao "[0-9]*\.[0-9]*%" /tmp/restic_progress | tail -1)"
   echo "Backup: ${progress:-"..."}"
 else
   echo
